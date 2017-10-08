41339934f452022048d95e36b6de78a7f5e5ea20
Nick Fox-Gieg <nick@fox-gieg.com>
2017-10-08 16:20:43 -0400

    new test

---
+++ move2/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class CameraFollow : NetworkBehaviour {
+	
+	void Update () {
+		if (!isLocalPlayer) return;
+		
+		Camera.main.transform.LookAt(transform.position);
+	}
+
+}
