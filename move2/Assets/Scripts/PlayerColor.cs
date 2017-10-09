dcb651997a05767593df912c446760a9faeb1d1c
Nick Fox-Gieg <nick@fox-gieg.com>
2017-10-08 21:02:36 -0400

    cleanup

--- move2/Assets/Scripts/PlayerColor.cs
+++ move2/Assets/Scripts/PlayerColor.cs
@@ -7,12 +7,16 @@ using UnityEngine.Networking;
 
 public class PlayerColor : NetworkBehaviour {
 
+	private TextMesh textMesh;
+
 	private void Start() {
+		textMesh = GameObject.FindGameObjectWithTag("Text").GetComponent<TextMesh>();
+
 		if (isLocalPlayer) {
 			GetComponent<Renderer>().material.color = Color.blue;
 		}
 
-		Debug.Log("Player ID is " + Network.player.ToString());
+		textMesh.text = "Player ID is " + Network.player.ToString();
 	}
 
 }
